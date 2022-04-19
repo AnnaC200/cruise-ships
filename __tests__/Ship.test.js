620cc26e4acabce674a0e1afadff8b8a75af939a
Anna Chan <annachan200@gmail.com>
2022-04-19 13:57:26 +0100

    Ships have Itineraries that have Ports

--- __tests__/Ship.test.js
+++ __tests__/Ship.test.js
@@ -1,31 +1,60 @@
 const Port = require('../src/Port.js');
-const Ship = require('../src/Ship.js')
+const Ship = require('../src/Ship.js');
+const Itinerary = require('../src/Itinerary');
 
 //.toBeInstanceOf - checks that an object is an instance of a class i.e. Ship
 describe('ship constructor', () => {
     it('can be instantiated', () => {
-        expect(new Ship()).toBeInstanceOf(Object);
+        const port = new Port ('Dover');
+        const itinerary = new Itinerary ([port]);
+        const ship = new Ship(itinerary);
+
+        expect(Ship).toBeInstanceOf(Object);
     });  
 
+    it('has a starting port', () => {
+        const port = new Port('Dover');
+        const itinerary = new Itinerary([port]);
+        const ship = new Ship(itinerary);
+      
+        expect(ship.currentPort).toBe(port);
+    });
+
  // four-phase test 1. set -up, 2. exercise/ execute set up, 3.the result of the exercise is verified against the developer’s expectations. 
-    it('ship can set sail', () => {
+    it('can set sail', () => {
         const dover = new Port('Dover');
-        const ship = new Ship(dover);
-
+        const calais = new Port('Calais');
+        const itinerary = new Itinerary([dover, calais]);
+        const ship = new Ship(itinerary);
+    
         ship.setSail();
-
+    
         expect(ship.currentPort).toBeFalsy();
     });
 
     it('can dock at a different port', () => {
         const dover = new Port('Dover');
-        const ship = new Ship(dover);
-
         const calais = new Port('Calais');
-        ship.dock(calais);
-
+        const itinerary = new Itinerary([dover, calais])
+        const ship = new Ship(itinerary);
+      
+        ship.setSail();
+        ship.dock();
+      
         expect(ship.currentPort).toBe(calais);
-    })
+    });
+
+    it('can\'t sail further than its itinerary', () => {
+        const dover = new Port('Dover');
+        const calais = new Port('Calais');
+        const itinerary = new Itinerary([dover, calais]);
+        const ship = new Ship(itinerary);
+
+        ship.setSail();
+        ship.dock();
+
+        expect(() => ship.setSail()).toThrowError('End of itinerary reached');
+    });
 });
 
 
--- src/Ship.js
+++ src/Ship.js
@@ -1,18 +1,37 @@
-//defines a FUNCTION Ship 
-function Ship(port) {
-    this.currentPort = port;
-    }
+//defines a Ship constructor
+function Ship (itinerary) {
+    // pass an instance of ITINERARY into SHIP constructor.
+    this.itinerary = itinerary;
+    //Assign an ITINERARY property so we can keep accessing it. 
+    this.currentPort = itinerary.ports[0];
+    //retrieving ports via itinerary and using [] to access the first array
+    this.previousPort = null
+  }
 
 Ship.prototype.setSail = function () {
+    const itinerary = this.itinerary;
+    const currentPortIndex = itinerary.ports.indexOf(this.currentPort);
+
+    if (currentPortIndex === (itinerary.ports.length - 1)) {
+        throw new Error('End of itinerary reached');
+    }
+
+    this.previousPort = this.currentPort;
     this.currentPort = null;
 };
 
-Ship.prototype.dock = function (port) {
-    this.currentPort = port;
-};
+Ship.prototype.dock = function () {
+    const itinerary = this.itinerary;
+    const previousPortIndex = itinerary.ports.indexOf(this.previousPort);
+
+    this.currentPort = itinerary.ports[previousPortIndex + 1];
+}
+
+
 
 
 
 
 //exports the Ship FUNCTION from the file
 module.exports = Ship;
+
