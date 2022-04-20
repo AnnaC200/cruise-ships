0f6e000d0e6086eaf440340c7415f92a75d67d01
Anna Chan <annachan200@gmail.com>
2022-04-20 20:54:49 +0100

    ports have ships

--- __tests__/Port.test.js
+++ __tests__/Port.test.js
@@ -6,8 +6,25 @@ describe('port constructor', () => {
         expect(new Port()).toBeInstanceOf(Object);
     });  
 
-    it('port has name property', () => {
-        const port = new Port('Hull');
-        expect(port.name).toEqual('Hull');
+    it('can add a ship', () => {
+        const port = new Port('Dover');
+        const ship = {};
+
+        port.addShip(ship);
+
+        expect(port.ships).toContain(ship);
+    });
+
+    it('can remove a ship', () => {
+        const port = new Port ('Dover');
+        const titanic = {};
+        const bloodyMary = {};
+
+        port.addShip(titanic);
+        port.addShip(bloodyMary);
+        port.removeShip(bloodyMary);
+
+        expect(port.ships).toEqual([titanic]); 
     })
+
 })
