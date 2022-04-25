2f3f6405a609e8b65cf8daac7110462e35f0b733
Anna Chan <annachan200@gmail.com>
2022-04-25 20:47:52 +0100

    make it CRY

--- __tests__/Port.test.js
+++ __tests__/Port.test.js
@@ -2,29 +2,38 @@ const Port = require('../src/Port.js')
 
 
 describe('port constructor', () => {
+    describe ('with ports and itinerary', () => {
+        let ship; //using let outside of the beforeEach to assign variable
+        let dover;
+        let itinerary;
+
+        beforeEach(() => {
+            dover = new Port('Dover');
+        });
+
     it('can be instantiated', () => {
         expect(new Port()).toBeInstanceOf(Object);
-    });  
+    });
 
     it('can add a ship', () => {
-        const port = new Port('Dover');
+       
         const ship = {};
 
-        port.addShip(ship);
+       dover.addShip(ship);
 
-        expect(port.ships).toContain(ship);
+        expect(dover.ships).toContain(ship);
     });
 
     it('can remove a ship', () => {
-        const port = new Port ('Dover');
+        
         const titanic = {};
         const bloodyMary = {};
 
-        port.addShip(titanic);
-        port.addShip(bloodyMary);
-        port.removeShip(bloodyMary);
+        dover.addShip(titanic);
+        dover.addShip(bloodyMary);
+        dover.removeShip(bloodyMary);
 
-        expect(port.ships).toEqual([titanic]); 
+        expect(dover.ships).toEqual([titanic]); 
     })
-
+    })
 })
